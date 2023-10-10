3a98dd5aa5e2a552f1852350b9aa0ef4277867a0
Diamantis Kourkouzelis <diamantis@endor.ai>
2023-10-10 16:01:26 -0700

    Use version metadata to download binary

--- Formula/endorctl.rb
+++ Formula/endorctl.rb
@@ -1,37 +1,79 @@
+require 'net/http'
+
 class Endorctl < Formula
 
-  desc "Endor Labs CLI"
-
   homepage "https://github.com/endorlabs/homebrew-tap"
+  desc "Endor Labs Homebrew Tap"
 
-  version "v1.5.260"
-
-  on_macos do
-    on_arm do
-      url "https://api.staging.endorlabs.com/download/endorlabs/#{version}/binaries/endorctl_#{version}_macos_arm64"
-      sha256 "c169052a595e5b4e0c6eabaa719371da19b3e7e1d6fb18a9b771f8d4e8a9a8bf"
-    end
-    on_intel do
-      url "https://api.staging.endorlabs.com/download/endorlabs/#{version}/binaries/endorctl_#{version}_macos_amd64"
-      sha256 "0ef1ff4152cdede028017cda087b226c174631eaa966400f95aa66a924d1f50c"
-    end
-  end
-
-
-  #license "MIT"
+  url "https://api.endorlabs.com/meta/version"
+  version "latest"
 
   def install
+    version_uri = URI("https://api.endorlabs.com/meta/version")
+
+    # Create an HTTP object and enable SSL for HTTPS
+    http = Net::HTTP.new(version_uri.host, version_uri.port)
+    http.use_ssl = (version_uri.scheme == "https")
+
+    # Create an HTTP GET request
+    req = Net::HTTP::Get.new(version_uri)
+
+    # Send the GET request and store the response
+    res = http.request(req)
+    if res.is_a?(Net::HTTPSuccess)
+      json_data = JSON.parse(res.body)
+      version = json_data["Service"]["Version"]
+      puts "Latest version: #{version}"
+    else
+      ohai "Failed to get version metadata"
+      exit 1
+    end
+
+    # Assume MacOS, ARM
+    use_arch = "arm64"
+    sha_value = json_data["ClientChecksums"]["ARCH_TYPE_MACOS_ARM64"]
+
     on_macos do
-      on_arm do
-        bin.install "endorctl_#{version}_macos_arm64" => "endorctl"
-      end
       on_intel do
-        bin.install "endorctl_#{version}_macos_amd64" => "endorctl"
+        use_arch = "amd64"
+        sha_value = json_data["ClientChecksums"]["ARCH_TYPE_MACOS_AMD64"]
       end
+    end
+
+    # Download URI
+    endorctl_file = "endorctl_#{version}_macos_#{use_arch}"
+    download_uri = URI("https://api.endorlabs.com/download/endorlabs/#{version}/binaries/#{endorctl_file}")
+
+    # Create an HTTP object and enable SSL for HTTPS
+    http = Net::HTTP.new(download_uri.host, download_uri.port)
+    http.use_ssl = (download_uri.scheme == "https")
+
+    # Create an HTTP GET request
+    req = Net::HTTP::Get.new(download_uri)
+
+    # Send the GET request and store the response
+    res = http.request(req)
+    if res.is_a?(Net::HTTPSuccess)
+      # Save the downloaded file to your download directory
+      File.open("#{endorctl_file}", "wb") do |file|
+        file.write(res.body)
+      end
+    else
+      ohai "Failed to download binary"
+      exit 1
+    end
+    sha256_value = `shasum -a 256 #{endorctl_file}`.split.first
+
+    if sha256_value != sha_value
+      ohai "SHA mismatch: expected #{sha256_value}, got #{sha_value}"
+      exit 1
+    else
+      bin.install "#{endorctl_file}" => "endorctl"
     end
   end
 
   test do
     system "#{bin}/endorctl", "--version"
   end
+
 end
