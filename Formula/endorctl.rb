15a60ac6c199bc2fa4a6e909f616d86989492c64
Diamantis Kourkouzelis <diamantis@endor.ai>
2023-10-11 13:53:13 -0700

    Use download strategy

--- Formula/endorctl.rb
+++ Formula/endorctl.rb
@@ -1,68 +1,54 @@
 require 'net/http'
 
+class EndorDownloadStrategy < CurlDownloadStrategy
+  def url
+    versioned_url = $download_url
+    versioned_url
+  end
+end
+
 class Endorctl < Formula
 
   homepage "https://github.com/endorlabs/homebrew-tap"
   desc "Endor Labs Homebrew Tap"
-
-  url "https://api.endorlabs.com/meta/version"
   version "latest"
 
+  # Get latest version metadata
+  version_uri = URI("https://api.endorlabs.com/meta/version")
+  http = Net::HTTP.new(version_uri.host, version_uri.port)
+  http.use_ssl = true
+
+  req = Net::HTTP::Get.new(version_uri)
+  res = http.request(req)
+  if res.is_a?(Net::HTTPSuccess)
+    json_data = JSON.parse(res.body)
+    version = json_data["Service"]["Version"]
+    puts "Latest version: #{version}"
+  else
+    ohai "Failed to get version metadata"
+    exit 1
+  end
+
+  # Assume MacOS, ARM
+  use_arch = "arm64"
+  $download_sha = json_data["ClientChecksums"]["ARCH_TYPE_MACOS_ARM64"]
+
+  on_macos do
+    on_intel do
+      use_arch = "amd64"
+      $download_sha = json_data["ClientChecksums"]["ARCH_TYPE_MACOS_AMD64"]
+    end
+  end
+
+  $endorctl_file = "endorctl_#{version}_macos_#{use_arch}"
+  $download_url = "https://api.endorlabs.com/download/endorlabs/#{version}/binaries/#{$endorctl_file}"
+
+  # Download and install
+  url "https://api.endorlabs.com/meta/version", :using => EndorDownloadStrategy
+  sha256 $download_sha
+
   def install
-
-    # Version URI
-    version_uri = URI("https://api.endorlabs.com/meta/version")
-
-    http = Net::HTTP.new(version_uri.host, version_uri.port)
-    http.use_ssl = true
-
-    req = Net::HTTP::Get.new(version_uri)
-    res = http.request(req)
-    if res.is_a?(Net::HTTPSuccess)
-      json_data = JSON.parse(res.body)
-      version = json_data["Service"]["Version"]
-      puts "Latest version: #{version}"
-    else
-      ohai "Failed to get version metadata"
-      exit 1
-    end
-
-    # Assume MacOS, ARM
-    use_arch = "arm64"
-    sha_value = json_data["ClientChecksums"]["ARCH_TYPE_MACOS_ARM64"]
-
-    on_macos do
-      on_intel do
-        use_arch = "amd64"
-        sha_value = json_data["ClientChecksums"]["ARCH_TYPE_MACOS_AMD64"]
-      end
-    end
-
-    # Download URI
-    endorctl_file = "endorctl_#{version}_macos_#{use_arch}"
-    download_uri = URI("https://api.endorlabs.com/download/endorlabs/#{version}/binaries/#{endorctl_file}")
-
-    http = Net::HTTP.new(download_uri.host, download_uri.port)
-    http.use_ssl = true
-
-    req = Net::HTTP::Get.new(download_uri)
-    res = http.request(req)
-    if res.is_a?(Net::HTTPSuccess)
-      File.open("#{endorctl_file}", "wb") do |file|
-        file.write(res.body)
-      end
-    else
-      ohai "Failed to download binary"
-      exit 1
-    end
-    sha256_value = `shasum -a 256 #{endorctl_file}`.split.first
-
-    if sha256_value != sha_value
-      ohai "Checksum mismatch: expected #{sha256_value}, got #{sha_value}"
-      exit 1
-    else
-      bin.install "#{endorctl_file}" => "endorctl"
-    end
+     bin.install "#{$endorctl_file}" => "endorctl"
   end
 
   test do
