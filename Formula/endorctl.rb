04d6be3f6f31bead7a044d48de10b0f3b4bf7e19
Diamantis Kourkouzelis <diamantis@endor.ai>
2023-10-10 17:38:21 -0700

    cleanup

--- Formula/endorctl.rb
+++ Formula/endorctl.rb
@@ -9,16 +9,14 @@ class Endorctl < Formula
   version "latest"
 
   def install
+
+    # Version URI
     version_uri = URI("https://api.endorlabs.com/meta/version")
 
-    # Create an HTTP object and enable SSL for HTTPS
     http = Net::HTTP.new(version_uri.host, version_uri.port)
-    http.use_ssl = (version_uri.scheme == "https")
+    http.use_ssl = true
 
-    # Create an HTTP GET request
     req = Net::HTTP::Get.new(version_uri)
-
-    # Send the GET request and store the response
     res = http.request(req)
     if res.is_a?(Net::HTTPSuccess)
       json_data = JSON.parse(res.body)
@@ -44,17 +42,12 @@ class Endorctl < Formula
     endorctl_file = "endorctl_#{version}_macos_#{use_arch}"
     download_uri = URI("https://api.endorlabs.com/download/endorlabs/#{version}/binaries/#{endorctl_file}")
 
-    # Create an HTTP object and enable SSL for HTTPS
     http = Net::HTTP.new(download_uri.host, download_uri.port)
-    http.use_ssl = (download_uri.scheme == "https")
+    http.use_ssl = true
 
-    # Create an HTTP GET request
     req = Net::HTTP::Get.new(download_uri)
-
-    # Send the GET request and store the response
     res = http.request(req)
     if res.is_a?(Net::HTTPSuccess)
-      # Save the downloaded file to your download directory
       File.open("#{endorctl_file}", "wb") do |file|
         file.write(res.body)
       end
@@ -65,7 +58,7 @@ class Endorctl < Formula
     sha256_value = `shasum -a 256 #{endorctl_file}`.split.first
 
     if sha256_value != sha_value
-      ohai "SHA mismatch: expected #{sha256_value}, got #{sha_value}"
+      ohai "Checksum mismatch: expected #{sha256_value}, got #{sha_value}"
       exit 1
     else
       bin.install "#{endorctl_file}" => "endorctl"
